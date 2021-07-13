5f780c13ec5a86ad1d5c7ef7919a075f0458f201
BrenoGT <30707133+BrenoGT@users.noreply.github.com>
2021-07-12 23:26:37 -0300

    Add files via upload

--- acesso_p3.py
+++ acesso_p3.py
@@ -1,11 +1,27 @@
 import requests
-from utils import json
+import json
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
-cat_file = r'K:\di estrategia_riscos\Riscos Financeiros\Relatorio Pilar 3 - Circ. 3.930\Relatório e Tabelas\Programa Python - Pilar 3\JSON\API Files\catalogo\catalogo.json'
+cat_file = r''
+
+
+def read_json(file_path: str, encoding: str = 'utf8') -> json:
+    """
+    Reads a JSON file
+
+    :param file_path: the path os the file
+    :param encoding: (optional) the encoding used in the reading
+    :return: the JSON data
+    """
+    with open(file_path, 'r', encoding=encoding) as file:
+        data = json.load(file)
+        file.close()
+    return data
 
 
 def main():
-    data = json.read_json(cat_file)
+    requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+    data = read_json(cat_file)
     recursos = data['conjuntos'][0]['recursos']
     url_base = data['conjuntos'][0]['urlBase']
     for rec in recursos:
@@ -15,13 +31,20 @@ def main():
         for d in rec['parametros']['valores']:
             resto_url = caminho.replace('{%s}' % nome_sub, d)
             caminho_completo = url_base + resto_url + '/' + nome.lower() + '.json'
-            print(f'Acessando: {caminho_completo}')
             try:
-                api = requests.get(caminho).json()
+                api = requests.get(caminho_completo, verify=False).json()
                 if len(api) < 1:
-                    print('A API do ')
-            except Exception:
-                pass
+                    print(f'Link: {caminho_completo}')
+                    print(f'A API do {nome} está vazia.\n')
+            except json.JSONDecodeError as json_err:
+                print(f'Link: {caminho_completo}')
+                print('Erro ao ler o JSON\n')
+            except Exception as err:
+                print(f'Link: {caminho_completo}')
+                print('Não foi possível ler a API.')
+                print(err, end='\n\n')
+            else:
+                print(f'Acesso a {nome} está ok!')
 
 
 if __name__ == '__main__':
