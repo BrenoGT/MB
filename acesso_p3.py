e6939da15dbd218b1de5cb2596cac7fda201f24c
BrenoGT <30707133+BrenoGT@users.noreply.github.com>
2021-07-15 09:16:11 -0300

    Add files via upload

--- acesso_p3.py
+++ acesso_p3.py
@@ -2,7 +2,7 @@ import requests
 import json
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
-cat_file = r''
+cat_file = r'C:\Users\Breno\Documents\Pessoal\TI\Linguagens\Python\Workspace_Python\MB\p3\catalogo.json'
 
 
 def read_json(file_path: str, encoding: str = 'utf8') -> json:
@@ -21,30 +21,35 @@ def read_json(file_path: str, encoding: str = 'utf8') -> json:
 
 def main():
     requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
-    data = read_json(cat_file)
-    recursos = data['conjuntos'][0]['recursos']
-    url_base = data['conjuntos'][0]['urlBase']
-    for rec in recursos:
-        nome = rec['nome'].split(' - ')[0]
-        caminho = rec['caminhoRecurso']
-        nome_sub = rec['parametros']['nome']
-        for d in rec['parametros']['valores']:
-            resto_url = caminho.replace('{%s}' % nome_sub, d)
-            caminho_completo = url_base + resto_url + '/' + nome.lower() + '.json'
-            try:
-                api = requests.get(caminho_completo, verify=False).json()
-                if len(api) < 1:
+    try:
+        data = read_json(cat_file)
+    except FileNotFoundError:
+        print(f'Não foi possível encontrar o arquivo.\nCaminho: {cat_file}')
+    else:
+        recursos = data['conjuntos'][0]['recursos']
+        url_base = data['conjuntos'][0]['urlBase']
+        for rec in recursos:
+            nome = rec['nome'].split(' - ')[0]
+            caminho = rec['caminhoRecurso']
+            nome_sub = rec['parametros']['nome']
+            for d in rec['parametros']['valores']:
+                resto_url = caminho.replace('{%s}' % nome_sub, d)
+                caminho_completo = url_base + resto_url + '/' + nome.lower() + '.json'
+                try:
+                    api = requests.get(caminho_completo, verify=False).json()
+                    if len(api) < 1:
+                        print(f'Link: {caminho_completo}')
+                        print(f'A API do {nome} está vazia.\n')
+                except json.JSONDecodeError as json_err:
                     print(f'Link: {caminho_completo}')
-                    print(f'A API do {nome} está vazia.\n')
-            except json.JSONDecodeError as json_err:
-                print(f'Link: {caminho_completo}')
-                print('Erro ao ler o JSON\n')
-            except Exception as err:
-                print(f'Link: {caminho_completo}')
-                print('Não foi possível ler a API.')
-                print(err, end='\n\n')
-            else:
-                print(f'Acesso a {nome} está ok!')
+                    print('Erro ao ler o JSON\n')
+                    print(json_err, end='\n\n')
+                except Exception as err:
+                    print(f'Link: {caminho_completo}')
+                    print('Não foi possível ler a API.')
+                    print(err, end='\n\n')
+                else:
+                    print(f'Acesso a {nome} está ok!')
 
 
 if __name__ == '__main__':
